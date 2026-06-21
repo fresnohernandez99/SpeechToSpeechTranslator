18d2c0a06312021df8e7a66a13a0dad7c9f3aad7
Eddy Yoel Fresno Hdez <fresnohernandez99@gmail.com>
2026-06-20 20:36:07 -0600

    stabilization fixes to screen parameters

--- sharedUI/src/commonMain/kotlin/com/fresnohernandez99/stpt/presentation/navigation/Destination.kt
+++ sharedUI/src/commonMain/kotlin/com/fresnohernandez99/stpt/presentation/navigation/Destination.kt
@@ -1,17 +1,24 @@
 package com.fresnohernandez99.stpt.presentation.navigation
 
+import androidx.compose.runtime.Immutable
 import kotlinx.serialization.Serializable
 
 @Serializable
+@Immutable
 sealed interface Destination {
     @Serializable
+    @Immutable
     data class Home(val typed: String = "") : Destination
     @Serializable
+    @Immutable
     data object Settings : Destination
     @Serializable
+    @Immutable
     data object ModelSelection : Destination
     @Serializable
+    @Immutable
     data class Transcription(val audioPath: String = "") : Destination
     @Serializable
+    @Immutable
     data object DictsManage : Destination
 }
