464bd0e59e7ca7feb2ee6fa8eb681f5de0519241
Eddy Yoel Fresno Hdez <fresnohernandez99@gmail.com>
2026-06-20 21:30:34 -0600

    modified typography to adjust new figma style

--- sharedUI/src/commonMain/kotlin/com/fresnohernandez99/stpt/theme/Type.kt
+++ sharedUI/src/commonMain/kotlin/com/fresnohernandez99/stpt/theme/Type.kt
@@ -2,143 +2,142 @@ package com.fresnohernandez99.stpt.theme
 
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.Font
+import speechtospeechtranslator.sharedui.generated.resources.Res
+import speechtospeechtranslator.sharedui.generated.resources.sf_pro_display_bold
+import speechtospeechtranslator.sharedui.generated.resources.sf_pro_display_medium
+import speechtospeechtranslator.sharedui.generated.resources.sf_pro_display_regular
+import speechtospeechtranslator.sharedui.generated.resources.sf_pro_display_semibold
 
-fun appTypography(colorScheme: ColorScheme) = Typography(
-    bodySmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
-
-    titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 16.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 18.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 22.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
-
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 14.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
-    ),
-    labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 16.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
-    ),
-    labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 18.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
-    )
+@Composable
+fun sfProDisplayFontFamily() = FontFamily(
+    Font(resource = Res.font.sf_pro_display_regular, weight = FontWeight.Normal),
+    Font(resource = Res.font.sf_pro_display_medium, weight = FontWeight.Medium),
+    Font(resource = Res.font.sf_pro_display_bold, weight = FontWeight.Bold),
+    Font(resource = Res.font.sf_pro_display_semibold, weight = FontWeight.SemiBold)
 )
 
-fun appTypographyBig(colorScheme: ColorScheme) = Typography(
-    bodySmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        letterSpacing = 0.5.sp,
-        color = colorScheme.onBackground,
-    ),
+@Composable
+fun appTypography(colorScheme: ColorScheme): Typography {
+    val sfPro = sfProDisplayFontFamily()
+    return Typography(
+        // H1 Title
+        titleLarge = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Bold,
+            fontSize = 54.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
 
-    titleSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 20.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 22.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W600,
-        fontSize = 26.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.sp
-    ),
+        // H2
+        titleMedium = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 40.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
 
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 18.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
-    ),
-    labelMedium = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 20.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
-    ),
-    labelLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.ExtraLight,
-        fontSize = 22.sp,
-        color = colorScheme.onBackground,
-        letterSpacing = 0.5.sp
+        // H3
+        titleSmall = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Medium,
+            fontSize = 32.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
+
+        // H4
+        bodyLarge = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Normal,
+            fontSize = 24.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
+
+        // H5
+        bodyMedium = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Light,
+            fontSize = 16.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
+
+        // H6
+        bodySmall = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Light,
+            fontSize = 8.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
     )
-)
+}
+
+@Composable
+fun appTypographyBig(colorScheme: ColorScheme): Typography {
+    val sfPro = sfProDisplayFontFamily()
+    return Typography(
+        // H1 Title
+        titleLarge = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Bold,
+            fontSize = 60.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
+
+        // H2
+        titleMedium = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 46.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
+
+        // H3
+        titleSmall = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Medium,
+            fontSize = 38.sp,
+            color = colorScheme.onBackground,
+            letterSpacing = 0.sp
+        ),
+
+        // H4
+        bodyLarge = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Normal,
+            fontSize = 30.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
+
+        // H5
+        bodyMedium = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Light,
+            fontSize = 22.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
+
+        // H6
+        bodySmall = TextStyle(
+            fontFamily = sfPro,
+            fontWeight = FontWeight.Light,
+            fontSize = 14.sp,
+            letterSpacing = 0.sp,
+            color = colorScheme.onBackground,
+        ),
+    )
+}
